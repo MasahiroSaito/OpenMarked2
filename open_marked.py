2dcc029b368d669028e81229e5b3cd6f3455de6d
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2016-07-21 09:52:48 +0900

    [2016.07.21] ファイルが Markdown ファイルではなかった場合にメッセージを表示

--- open_marked.py
+++ open_marked.py
@@ -2,4 +2,8 @@ import sublime, sublime_plugin, os
 
 class OpenMarkedCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		os.system('open -a \"Marked 2\" ' + self.view.file_name())
+		path = self.view.file_name()
+		if path.endswith(".md"):
+			os.system('open -a \"Marked 2\" ' + self.view.file_name())
+		else:
+			sublime.message_dialog("not markdown file")
