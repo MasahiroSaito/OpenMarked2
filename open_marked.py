7d6378868ee2f83a131b067377d68b799dfb95cc
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2016-11-17 11:26:28 +0900

    ファイル名にスペースがあっても開けるよう修正

--- open_marked.py
+++ open_marked.py
@@ -4,6 +4,6 @@ class OpenMarkedCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		path = self.view.file_name()
 		if path.endswith(".md"):
-			os.system('open -a \"Marked 2\" ' + self.view.file_name())
+			os.system('open -a \"Marked 2\" \"' + self.view.file_name() + "\"")
 		else:
 			sublime.message_dialog("not markdown file")
